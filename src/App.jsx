4a40b81405e22424639c807fde8a85ce83ae4435
Aditya Singh <adityasingh740745@gmail.com>
2024-10-26 20:41:37 +0530

    feat: add data in localstorage and created getter and setter fns

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard/EmployeeDashboard"
 import AdminDashboard from "./components/Dashboard/AdminDashboard/AdminDashboard"
+import { useEffect } from "react";
+import { setLocalStorage, getLocalStorage } from "./utils/LocalStorage";
 
 const App = () => {
+
+    useEffect(() => {
+        setLocalStorage()
+        getLocalStorage()
+    }, [])
+
     return (
         <div>
             <Login />
